d9dc89ba9336322a736d8f04caae19ca70913ffe
soies <soiesnepal70@gmail.com>
2026-02-14 21:07:18 +0545

    Final fix: Bypassed next-sanity imports in live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,11 +1,7 @@
-// Temporarily disabled to fix Vercel build error 'getDraftId'
-// import { defineLive } from "next-sanity";
 import { client } from './client'
 
-// We create a dummy sanityFetch that just uses the regular client
 export const sanityFetch = async ({ query, params = {} }: any) => {
   return client.fetch(query, params);
 };
 
-// We create a dummy component so your layout doesn't crash
 export const SanityLive = () => null;
